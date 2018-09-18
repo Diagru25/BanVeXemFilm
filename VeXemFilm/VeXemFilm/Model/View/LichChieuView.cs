f4ab550911144264a59c1d9d6ea5846c5dd8fb95
huygiangit96 <32055253+huygiangit96@users.noreply.github.com>
2018-09-18 10:34:20 +0700

    LichChieu

---
+++ VeXemFilm/VeXemFilm/Model/View/LichChieuView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VeXemFilm.Model.View
+{
+    public class LichChieuView
+    {
+        public long ID { get; set; }
+        public TimeSpan ThoiGianBatDau { get; set; }
+        public TimeSpan ThoiGianKetThuc { get; set; }
+        public DateTime NgayChieu { get; set; }
+        public long IDPhim { get; set; }
+        public string TenPhim { get; set; }
+        public long IDPhongChieu { get; set; }
+        public string TenPhongChieu { get; set; }
+        public Decimal GiaVe { get; set; }
+    }
+}
